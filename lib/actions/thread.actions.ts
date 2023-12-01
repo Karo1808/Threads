3aef186b914e218ad842cff48a84839487b51bfa
Karo <karolp2003@gmail.com>
2023-12-01 10:05:35 +0100

    Home Page Functionality

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -20,16 +20,13 @@ export async function createThread({
   path,
 }: Params) {
   try {
-    connectToDB();
+    await connectToDB();
 
-    console.log("------------------------------\n", author);
     const createdThread = await Thread.create({
       text,
       author,
       community: null,
     });
-    console.log("===================================");
-    console.log(createdThread);
 
     // Update user model
     await User.findByIdAndUpdate(author, {
@@ -39,4 +36,42 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
   revalidatePath(path);
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    await connectToDB();
+
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts that have no parents
+    const postQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch posts: ${error.message}`);
+  }
 }
