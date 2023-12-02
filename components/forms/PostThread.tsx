260ad1fd656d55fdeb3b7d331ca082e42f52490b
Karo <karolp2003@gmail.com>
2023-12-02 11:29:27 +0100

    Communities feature beta

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -22,6 +22,7 @@ import { useRouter } from "next/navigation";
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 
 interface AccountProfileProps {
   user: {
@@ -38,6 +39,7 @@ interface AccountProfileProps {
 const PostThread = ({ userId }: { userId: string }) => {
   const pathname = usePathname();
   const router = useRouter();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -51,7 +53,7 @@ const PostThread = ({ userId }: { userId: string }) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization?.id || null,
       path: pathname,
     });
 
